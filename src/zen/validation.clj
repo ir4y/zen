7a707a7a4077a7c6721f6629879949c380010e55
ApricotLace <profunctorlense@gmail.com>
2021-08-18 17:49:31 +0300

    Fix date/datetime regex
    Co-authored-by: @yurii_makhotkin <kgofhedgehogs@gmail.com>

--- src/zen/validation.clj
+++ src/zen/validation.clj
@@ -457,13 +457,13 @@
 
 (defmethod validate-type 'zen/date
   [_ ctx acc schema data]
-  (if (and (string? data) (re-matches #"\d{4}-(0[1-9]|1[012])-([012][1-9]|3[01])" data))
+  (if (and (string? data) (re-matches #"([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?"  data))
     acc
     (add-error ctx acc {:message (format "Expected type of 'date, got \"%s\"" data) :type "primitive-type"})))
 
 (defmethod validate-type 'zen/datetime
   [_ ctx acc schema data]
-  (if (and (string? data) (re-matches #"(?i)\d{4}-(0[1-9]|1[012])-([012][1-9]|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)[Z]?" data))
+  (if (and (string? data) (re-matches #"(?i)([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?" data))
     acc
     (add-error ctx acc {:message (format "Expected type of 'datetime, got \"%s\"" data) :type "primitive-type"})))
 
